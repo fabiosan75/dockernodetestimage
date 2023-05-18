9bced3fedb1f00bc9d6abd5aa5a8c53fce347f0b
fabiosan75 <fabiosan75@gmail.com>
2023-05-18 12:26:10 -0500

    Depuracion archivos

--- Actiividad3_FabioAndresSanchezBernal/app/mongoconn.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://app_user:app_password@mongoDB:27017/test', {
-    
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useNewUrlParser: true
-    }).then(db => console.log('conexion exitosa'))
-    .catch(err => console.log('error: ', err))
